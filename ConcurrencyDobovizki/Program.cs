e745c2fbd0145fbfd07c13d32bd433acaaa77930
ebranquinho <branquinhomanu@gmail.com>
2025-04-26 10:50:34 -0300

    wip manual reset event slim

--- ConcurrencyDobovizki/Program.cs
+++ ConcurrencyDobovizki/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.ReadKey();
+﻿var myEvent = new ManualResetEventSlim(false);
+var threadWeWaitFor = new Thread(()=>
+{
+Console.WriteLine("Doing something");
+Thread.Sleep(5000);
+Console.WriteLine("Finished");
+myEvent.Set();
+});
+var waitingThread = new Thread(()=>
+{
+Console.WriteLine("Waiting for other thread to do something");
+myEvent.Wait();
+Console.WriteLine("Other thread finished, we can continue");
+});
+threadWeWaitFor.Start();
+waitingThread.Start();
+
+Console.ReadKey();
